bd5497681d9a595b125e3986fff44ac93d048f2d
guites <gui.garcia67@gmail.com>
2026-03-18 14:47:38 -0300

    leader d to show floating diagnostic window

--- init.lua
+++ init.lua
@@ -195,6 +195,7 @@ vim.diagnostic.config {
 }
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>d', function() vim.diagnostic.open_float() end, { desc = 'Diagnostic float' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
